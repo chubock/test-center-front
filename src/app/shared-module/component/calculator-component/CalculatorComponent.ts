6838922b6d752fc169e92e1f9c0511a66688c664
yubar persia <yubar45@gmail.com>
2017-04-24 00:13:25 +0430

    calculator started

---
+++ src/app/shared-module/component/calculator-component/CalculatorComponent.ts
@@ -0,0 +1,13 @@
+import {Component} from "@angular/core";
+/**
+ * Created by Yubar on 4/23/2017.
+ */
+
+
+@Component({
+  selector: 'calculator',
+  styles: ['.calc-btn {width: 30px;height: 30px;margin: 1px;padding: 1px;}']
+})
+export class CalculatorComponent {
+
+}
